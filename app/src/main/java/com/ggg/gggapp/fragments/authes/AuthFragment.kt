63d697bed4c90cbcdc2e10e837a66012e177c01a
Kozlov.M.V <3maxmain3@gmail.com>
2022-04-09 20:36:59 +0500

    regComplite

--- app/src/main/java/com/ggg/gggapp/fragments/authes/AuthFragment.kt
+++ app/src/main/java/com/ggg/gggapp/fragments/authes/AuthFragment.kt
@@ -12,26 +12,34 @@ import com.ggg.gggapp.R
 import com.ggg.gggapp.activities.BottomNavigationActivity
 import com.ggg.gggapp.auth.Auth
 import com.ggg.gggapp.databinding.FragmentAuthBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class AuthFragment : Fragment() {
 
     private lateinit var binding: FragmentAuthBinding
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAuthBinding.inflate(inflater)
+        auth = Firebase.auth
         binding.RegButton.setOnClickListener{
-            requireActivity().supportFragmentManager.beginTransaction().replace(R.id.container_Fragment, RegFragment()).commit()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.container_Fragment, RegFragment()).commit()
         }
         binding.AuthButton.setOnClickListener{
-            if(!binding.Login.text.toString().isEmpty() && !binding.Login.text.toString().isEmpty()){
-                Auth().auth(binding.Login.text.toString(),binding.Login.text.toString())
-                requireActivity().startActivity(Intent(requireActivity(),BottomNavigationActivity::class.java))
-                requireActivity().finish()
+            if(!binding.Login.text.toString().isEmpty() && !binding.Password.text.toString().isEmpty()){
+                auth.signInWithEmailAndPassword(binding.Login.text.toString() ,binding.Password.text.toString()).addOnCompleteListener{
+                       if(it.isSuccessful){
+                           requireActivity().startActivity(Intent(requireActivity(),BottomNavigationActivity::class.java))
+                           requireActivity().finish()
+                       }
+                }
+
             }
             else{
                 Toast.makeText(activity,"Вы не ввели логин или пароль",Toast.LENGTH_SHORT).show()
