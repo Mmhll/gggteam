18a792f2fbb664abc0afdb27b1f636ddf08753ae
Kozlov.M.V <3maxmain3@gmail.com>
2022-04-09 22:32:36 +0500

    regComplite

--- app/src/main/java/com/ggg/gggapp/fragments/authes/RegFragment.kt
+++ app/src/main/java/com/ggg/gggapp/fragments/authes/RegFragment.kt
@@ -1,5 +1,7 @@
 package com.ggg.gggapp.fragments.authes
 
+import android.app.Activity.RESULT_OK
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -11,6 +13,7 @@ import com.ggg.gggapp.activities.BottomNavigationActivity
 import com.ggg.gggapp.database.Database
 import com.ggg.gggapp.databinding.FragmentRegBinding
 import com.ggg.gggapp.dataclasses.UserClass
+import com.ggg.gggapp.storage.Storage
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -19,6 +22,7 @@ class RegFragment : Fragment() {
 
     private lateinit var binding: FragmentRegBinding
     private lateinit var auth : FirebaseAuth
+    private lateinit var dataClass: UserClass
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,12 +30,21 @@ class RegFragment : Fragment() {
     ): View? {
         binding = FragmentRegBinding.inflate(inflater)
         auth = Firebase.auth
+        dataClass = UserClass()
+        binding.addImage.setOnClickListener {
+            AlertDialog.Builder(activity).setPositiveButton("Сделать снимок"){ p0, p1 ->
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                startActivityForResult(intent, 1)
+            }.setNegativeButton("Использовать ссылку"){ _,_ ->
+
+           }.create().show()
+        }
         binding.RegButton.setOnClickListener{
-            var dataClass = UserClass()
-            if(!binding.avatarText.text.toString().isEmpty() && !binding.emailText.text.toString().isEmpty()
+            if( !binding.emailText.text.toString().isEmpty() && binding.Check.isChecked
                 && !binding.nameText.text.toString().isEmpty() && !binding.patronymicText.text.toString().isEmpty()
                 && !binding.phoneNumberText.text.toString().isEmpty() && !binding.sexText.selectedItem.toString().isEmpty()
-                && !binding.surnameText.text.toString().isEmpty() && !binding.passwordText.text.toString().isEmpty() && binding.Check.isChecked){
+                && !binding.surnameText.text.toString().isEmpty() && !binding.passwordText.text.toString().isEmpty() ){
                 dataClass.avatar = binding.avatarText.text.toString()
                 dataClass.email = binding.emailText.text.toString()
                 dataClass.name = binding.nameText.text.toString()
@@ -56,5 +69,16 @@ class RegFragment : Fragment() {
             }
         }
         return binding.root
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+       super.onActivityResult(requestCode, resultCode, data)
+       when (requestCode){
+           1->{
+               if (resultCode == RESULT_OK){
+                   var uri = data?.data!!
+                   Storage().uploadImage(uri, dataClass)
+               }
+           }
+       }
     }
 }
