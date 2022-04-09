27daa69b68dae8d382f72d53a45c61bfa44a59d9
Kozlov.M.V <3maxmain3@gmail.com>
2022-04-09 18:59:24 +0500

    regCompliteSuka

--- app/src/main/java/com/ggg/gggapp/fragments/authes/RegFragment.kt
+++ app/src/main/java/com/ggg/gggapp/fragments/authes/RegFragment.kt
@@ -41,6 +41,8 @@ class RegFragment : Fragment() {
                 }
                 else{
                     Toast.makeText(activity, "Регистрация провалена", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity, "Регистрация провалена", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity, "Регистрация провалена", Toast.LENGTH_SHORT).show()
                 }
             }
             else{
