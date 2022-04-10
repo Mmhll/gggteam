ab43a9f4ad906a529e8da2c0ad5a6d0387b69f4d
Mmhll <bajduckov2014@gmail.com>
2022-04-10 08:06:23 +0500

    push

--- app/src/main/java/com/ggg/gggapp/fragments/services/ServicesFragment.kt
+++ app/src/main/java/com/ggg/gggapp/fragments/services/ServicesFragment.kt
@@ -1,6 +1,7 @@
 package com.ggg.gggapp.fragments.services
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import com.ggg.gggapp.databinding.FragmentServicesBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.R
 
 class ServicesFragment : Fragment() {
 
@@ -25,9 +27,14 @@ class ServicesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentServicesBinding.inflate(inflater, container, false)
-        auth = Firebase.auth
-        binding.logOut.setOnClickListener{
-            auth.signOut()
+
+        binding.hoursButton.setOnClickListener {
+            requireActivity().supportFragmentManager.beginTransaction()
+                .add(WorkTimeFragment(), "work")
+                .commit()
+                .let {
+                    Log.e("it", it.toString())
+                }
         }
         return binding.root
     }
---
+++ app/src/main/java/com/ggg/gggapp/fragments/services/WorkTimeFragment.kt
@@ -0,0 +1,24 @@
+package com.ggg.gggapp.fragments.services
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ggg.gggapp.R
+import com.ggg.gggapp.databinding.FragmentWorkTimeBinding
+
+class WorkTimeFragment : Fragment() {
+
+    private var binding : FragmentWorkTimeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentWorkTimeBinding.inflate(layoutInflater)
+        return binding!!.root
+    }
+
+
+}
