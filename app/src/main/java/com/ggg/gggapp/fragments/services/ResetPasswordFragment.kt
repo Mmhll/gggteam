06a255139ed7fa0b6d6e97ac098450026b90df85
Mmhll <bajduckov2014@gmail.com>
2022-04-10 10:35:16 +0500

    prelast commit

--- app/src/main/java/com/ggg/gggapp/fragments/services/ResetPasswordFragment.kt
+++ app/src/main/java/com/ggg/gggapp/fragments/services/ResetPasswordFragment.kt
@@ -1,6 +1,8 @@
 package com.ggg.gggapp.fragments.services
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +10,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.ggg.gggapp.R
 import com.ggg.gggapp.databinding.FragmentResetPasswordBinding
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class ResetPasswordFragment: Fragment() {
+class ResetPasswordFragment : Fragment() {
 
     private lateinit var binding: FragmentResetPasswordBinding
     private lateinit var auth: FirebaseAuth
@@ -23,16 +26,38 @@ class ResetPasswordFragment: Fragment() {
     ): View? {
         binding = FragmentResetPasswordBinding.inflate(inflater)
         auth = Firebase.auth
-        binding.resetPassword.setOnClickListener{
-            auth.currentUser?.updatePassword(binding.newPassword.text.toString())?.addOnCompleteListener{
-                if(it.isSuccessful){
-                    Toast.makeText(activity,"Пароль успешно обновлен", Toast.LENGTH_SHORT).show()
-                    requireActivity().supportFragmentManager.beginTransaction()
-                        .replace(com.ggg.gggapp.R.id.fragmentContainerServices, WorkTimeFragment()).commit()
-                }
-                else{
-                    Toast.makeText(activity,"Произошла чудовищная ошибка ${it.toString()}", Toast.LENGTH_SHORT).show()
+        binding.resetPassword.setOnClickListener {
+            /* auth.currentUser!!.updatePassword(binding.newPassword.text.toString()).addOnCompleteListener{
+                 if(it.isSuccessful){
+                     Toast.makeText(activity,"Пароль успешно обновлен", Toast.LENGTH_SHORT).show()
+                     requireActivity().supportFragmentManager.beginTransaction()
+                         .replace(R.id.layoutFragment, WorkTimeFragment()).commit()
+                 }
+                 else{
+                     Toast.makeText(activity,"Произошла чудовищная ошибка $it", Toast.LENGTH_SHORT).show()
 
+                 }
+             }*/
+            val prefs = requireActivity().getSharedPreferences("cred", Context.MODE_PRIVATE)
+            auth.currentUser!!.reauthenticate(
+                EmailAuthProvider.getCredential(
+                    prefs.getString("Email", "")!!,
+                    prefs.getString("Password", "")!!
+                )
+            ).addOnCompleteListener { it ->
+                if (it.isSuccessful) {
+                    auth.currentUser!!.updatePassword(binding.newPassword.text.toString())
+                        .addOnCompleteListener { it1 ->
+                            if (it1.isSuccessful) {
+                                Toast.makeText(requireContext(), "YEP", Toast.LENGTH_SHORT).show()
+                            } else {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "${it1.exception}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
                 }
             }
         }
