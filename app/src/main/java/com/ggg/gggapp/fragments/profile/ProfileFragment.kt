fd04db9a1d1cf8a311dd4f2e310e1cfe67eb8841
Mmhll <bajduckov2014@gmail.com>
2022-04-10 05:32:02 +0500

    уу

--- app/src/main/java/com/ggg/gggapp/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/ggg/gggapp/fragments/profile/ProfileFragment.kt
@@ -5,7 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.ggg.gggapp.database.Database
 import com.ggg.gggapp.databinding.FragmentProfileBinding
+import com.ggg.gggapp.dataclasses.UserClass
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 class ProfileFragment : Fragment() {
 
@@ -21,7 +29,31 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        var users = ArrayList<UserClass>()
+        Database().getFirebaseReference("Users").addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    for (snap in snapshot.children){
+                        var user = snap.getValue(UserClass::class.java)
+                        users.add(user!!)
+                    }
+                    var user : UserClass? = null
 
+                    for (i in users){
+                        if (i.uid == Firebase.auth.uid){
+                            user = i
+                            break
+                        }
+                    }
+
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
         return binding.root
     }
 
